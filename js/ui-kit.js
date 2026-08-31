ea267e5298ba26a8ec191da5a495a55933d48273
luyz2000 <luis2000_k999@hotmail.com>
2026-08-31 00:58:41 -0600

    Remove dead code and unused assets (audit point 1)
    - Remove js/plugins/ (bootstrap-datepicker, bootstrap-switch, jquery.sharrre, nouislider) unused in DOM
    - Purge styles/main.css (~1295 lines): bootstrap-switch, nouislider, datepicker styles
    - Simplify js/ui-kit.js (219->74 lines): keep only transparent navbar, toggler and debounce
    - Remove js/webp-fallback.js and its <script> tag in index.html (redundant with <picture> and @supports)
    - Remove duplicate preload for profile.jpg and data-parallax attribute from header
    - Savings: -5912 lines, -177KB plugins, main.css 196KB->134KB

--- js/ui-kit.js
+++ js/ui-kit.js
@@ -1,35 +1,16 @@
 var transparent = true;
-
-var transparentDemo = true;
-var fixedTop = false;
-
-var navbar_initialized,
-    backgroundOrange = false,
-    toggle_initialized = false;
+var scroll_distance = 400;
 
 $(document).ready(function () {
-    //  Activate the Tooltips
+    // Tooltips (usado en navbar-brand)
     $('[data-toggle="tooltip"], [rel="tooltip"]').tooltip();
 
-    // Activate Popovers and set color for popovers
-    $('[data-toggle="popover"]').each(function () {
-        color_class = $(this).data('color');
-        $(this).popover({
-            template: '<div class="popover popover-' + color_class + '" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
-        });
-    });
+    var $navbar = $('.navbar[color-on-scroll]');
+    scroll_distance = $navbar.attr('color-on-scroll') || 400;
 
-    // Activate the image for the navbar-collapse
-    nowuiKit.initNavbarImage();
-
-    $navbar = $('.navbar[color-on-scroll]');
-    scroll_distance = $navbar.attr('color-on-scroll') || 500;
-
-    // Check if we have the class "navbar-color-on-scroll" then add the function to remove the class "navbar-transparent" so it will transform to a plain color.
-
-    if ($('.navbar[color-on-scroll]').length != 0) {
+    if ($('.navbar[color-on-scroll]').length !== 0) {
         nowuiKit.checkScrollForTransparentNavbar();
-        $(window).on('scroll', nowuiKit.checkScrollForTransparentNavbar)
+        $(window).on('scroll', nowuiKit.checkScrollForTransparentNavbar);
     }
 
     $('.form-control').on("focus", function () {
@@ -37,70 +18,18 @@ $(document).ready(function () {
     }).on("blur", function () {
         $(this).parent(".input-group").removeClass("input-group-focus");
     });
-
-    // Activate bootstrapSwitch
-    $('.bootstrap-switch').each(function () {
-        $this = $(this);
-        data_on_label = $this.data('on-label') || '';
-        data_off_label = $this.data('off-label') || '';
-
-        $this.bootstrapSwitch({
-            onText: data_on_label,
-            offText: data_off_label
-        });
-    });
-
-    if ($(window).width() >= 992) {
-        big_image = $('.page-header-image[data-parallax="true"]');
-
-        $(window).on('scroll', nowuiKitDemo.checkScrollForParallax);
-    }
-
-    // Activate Carousel
-    $('.carousel').carousel({
-        interval: 4000
-    });
-
-    $('.date-picker').each(function () {
-        $(this).datepicker({
-            templates: {
-                leftArrow: '<i class="now-ui-icons arrows-1_minimal-left"></i>',
-                rightArrow: '<i class="now-ui-icons arrows-1_minimal-right"></i>'
-            }
-        }).on('show', function () {
-            $('.datepicker').addClass('open');
-
-            datepicker_color = $(this).data('datepicker-color');
-            if (datepicker_color.length != 0) {
-                $('.datepicker').addClass('datepicker-' + datepicker_color + '');
-            }
-        }).on('hide', function () {
-            $('.datepicker').removeClass('open');
-        });
-    });
-
-
-});
-
-$(window).on('resize', function () {
-    nowuiKit.initNavbarImage();
 });
 
 $(document).on('click', '.navbar-toggler', function () {
-    $toggle = $(this);
-
-    if (nowuiKit.misc.navbar_menu_visible == 1) {
+    var $toggle = $(this);
+    if (nowuiKit.misc.navbar_menu_visible === 1) {
         $('html').removeClass('nav-open');
         nowuiKit.misc.navbar_menu_visible = 0;
         $('#bodyClick').remove();
-        setTimeout(function () {
-            $toggle.removeClass('toggled');
-        }, 550);
+        setTimeout(function () { $toggle.removeClass('toggled'); }, 550);
     } else {
-        setTimeout(function () {
-            $toggle.addClass('toggled');
-        }, 580);
-        div = '<div id="bodyClick"></div>';
+        setTimeout(function () { $toggle.addClass('toggled'); }, 580);
+        var div = '<div id="bodyClick"></div>';
         $(div).appendTo('body').click(function () {
             $('html').removeClass('nav-open');
             nowuiKit.misc.navbar_menu_visible = 0;
@@ -109,17 +38,13 @@ $(document).on('click', '.navbar-toggler', function () {
                 $('#bodyClick').remove();
             }, 550);
         });
-
         $('html').addClass('nav-open');
         nowuiKit.misc.navbar_menu_visible = 1;
     }
 });
 
 nowuiKit = {
-    misc: {
-        navbar_menu_visible: 0
-    },
-
+    misc: { navbar_menu_visible: 0 },
     checkScrollForTransparentNavbar: debounce(function () {
         if ($(document).scrollTop() > scroll_distance) {
             if (transparent) {
@@ -132,83 +57,13 @@ nowuiKit = {
                 $('.navbar[color-on-scroll]').addClass('navbar-transparent');
             }
         }
-    }, 17),
-
-    initNavbarImage: function () {
-        var $navbar = $('.navbar').find('.navbar-translate').siblings('.navbar-collapse');
-        var background_image = $navbar.data('nav-image');
-
-        if ($(window).width() < 991 || $('body').hasClass('burger-menu')) {
-            if (background_image != undefined) {
-                $navbar.css('background', "url('" + background_image + "')")
-                    .removeAttr('data-nav-image')
-                    .css('background-size', "cover")
-                    .addClass('has-image');
-            }
-        } else if (background_image != undefined) {
-            $navbar.css('background', "")
-                .attr('data-nav-image', '' + background_image + '')
-                .css('background-size', "")
-                .removeClass('has-image');
-        }
-    },
-
-    initSliders: function () {
-        // Sliders for demo purpose in refine cards section
-        var slider = document.getElementById('sliderRegular');
-
-        noUiSlider.create(slider, {
-            start: 40,
-            connect: [true, false],
-            range: {
-                min: 0,
-                max: 100
-            }
-        });
-
-        var slider2 = document.getElementById('sliderDouble');
-
-        noUiSlider.create(slider2, {
-            start: [20, 60],
-            connect: true,
-            range: {
-                min: 0,
-                max: 100
-            }
-        });
-    }
-}
-
-
-var big_image;
-
-// Javascript just for Demo purpose, remove it from your project
-nowuiKitDemo = {
-    checkScrollForParallax: debounce(function () {
-        var current_scroll = $(this).scrollTop();
-
-        oVal = ($(window).scrollTop() / 3);
-        big_image.css({
-            'transform': 'translate3d(0,' + oVal + 'px,0)',
-            '-webkit-transform': 'translate3d(0,' + oVal + 'px,0)',
-            '-ms-transform': 'translate3d(0,' + oVal + 'px,0)',
-            '-o-transform': 'translate3d(0,' + oVal + 'px,0)'
-        });
-
-    }, 6)
-
-}
-
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing.
+    }, 17)
+};
 
 function debounce(func, wait, immediate) {
     var timeout;
     return function () {
-        var context = this,
-            args = arguments;
+        var context = this, args = arguments;
         clearTimeout(timeout);
         timeout = setTimeout(function () {
             timeout = null;
@@ -216,4 +71,4 @@ function debounce(func, wait, immediate) {
         }, wait);
         if (immediate && !timeout) func.apply(context, args);
     };
-};
+}
